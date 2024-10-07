299cf0af82123b9b6abb96e5b950d32ccf898ad5
YOGEESH K P <yogeesh793@gmail.com>
2024-10-07 21:01:38 +0530

    Add files via upload

---
+++ list_1.py
@@ -0,0 +1,8 @@
+# List is using for store int,float,string,complex and executing for single value
+List = [1,'Rahul',2.3,2,2+3j]
+print(List[2])
+
+# list using append add string
+bl = [1,2,3,4,5,6,5]
+bl.append("nandine")
+print(bl)
